ef3ded2e6307930367eb79a27b370c904c45f854
Narkoos <60522367+Narkoos@users.noreply.github.com>
2020-01-31 20:26:25 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,4 +2,4 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "!";
 
-client.login("NjcyODI0Mzg2MjkzODU4MzI1.XjRHiQ.mpkG11ttUNKKotgicTeUzimgEKY");
+bot.login(process.env.NjcyODI0Mzg2MjkzODU4MzI1.XjRHiQ.mpkG11ttUNKKotgicTeUzimgEKY);
